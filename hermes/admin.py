d3cf5bb22e8eff7b9ef141e0a9fdde47ac5482df
Emile TAWFIK <emile.tawfik@gmail.com>
2014-11-21 14:25:28 +0100

    Personalize the hermes admin

--- hermes/admin.py
+++ hermes/admin.py
@@ -5,11 +5,13 @@ from .models import Post, Category
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("subject", ), }
-
+    list_display = ('subject', 'category', 'created_on', 'modified_on')
+    list_filter = ('created_on', 'modified_on', 'category')
+    search_fields = ['subject', 'slug']
 
 class CategoryAdmin(admin.ModelAdmin):
-    include = ('title', 'parent', )
-    exclude = ('slug', )
+    include = ('title', 'parent', 'slug')
+    prepopulated_fields = {"slug": ("title", ), }
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category, CategoryAdmin)
