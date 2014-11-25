d74d892ff3a080267fd662a35ba4744b8fefe695
Emile TAWFIK <emile.tawfik@gmail.com>
2014-11-25 12:41:08 +0100

    Add Summernote

--- hermes/admin.py
+++ hermes/admin.py
@@ -1,18 +1,26 @@
 from django.contrib import admin
-
-from .models import Post, PostFile,Category
+from django.conf import settings
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Post, PostFile, Category
 
 class PostFileInline(admin.TabularInline):
     model = PostFile
     template = 'hermes/postfiles.html'
 
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     prepopulated_fields = {"slug": ("subject", ), }
     exclude = ('summary',)
     list_display = ('subject', 'category', 'created_on', 'modified_on')
     list_filter = ('created_on', 'modified_on', 'category')
     search_fields = ('subject', 'slug')
     inlines = (PostFileInline, )
+    class Media:
+        css = {
+            'all': (
+                "http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,700",
+                settings.STATIC_URL +  'css/admin_post.css',
+                ),
+        }
 
 class CategoryAdmin(admin.ModelAdmin):
     include = ('title', 'parent')
